001d134c5acba4b1df58e9f4b8fe180cf7dd4c22
Ubuntu <ubuntu@ip-172-31-26-84.us-west-2.compute.internal>
2019-06-24 02:24:36 +0000

    Fixed init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 echo "so ~/dotfiles/vim.dot" >> ~/.vimrc
 echo "source ~/dotfiles/zsh.dot" >> ~/.zshrc
 echo "source ~/dotfiles/tmux.dot" >> ~/.tmux.conf
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall
