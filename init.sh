9ebdc57d5ae3c0fcbbbcaebc6b488075dfdf422f
Matthew Hughes <matthewhughes3@tutanota.com>
2020-10-29 09:51:38 -0700

    Added xmodmap config and added it to init

--- init.sh
+++ init.sh
@@ -4,3 +4,5 @@ echo "source ~/dotfiles/tmux.dot" >> ~/.tmux.conf
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall
+cp ~/dotfiles/i3.dot ~/.i3/config
+cp ~/dotfiles/xmodmap.dot ~/.Xmodmap
